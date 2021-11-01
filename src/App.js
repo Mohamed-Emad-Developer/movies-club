430eb847cf1c6c5639322d3dd3a6b48ce94c3f09
Mohamed Emad <mohamed.emad.1999.0@gmail.com>
2021-11-01 21:23:09 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./layout/Header/Header";
 import { FaAngleDoubleUp } from "react-icons/fa";
 import Movies from "./components/Movies";
 const FEATURED_API =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=10";
 
 // const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=${Math.floor(
 //   Math.random() * 100 + 1
