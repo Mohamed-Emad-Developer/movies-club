d97af832dfe133926857870e038c5560cf168569
Mohamed Emad <mohamed.emad.1999.0@gmail.com>
2021-11-26 23:06:03 +0200

    changes3

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Footer.css";
-const date = new Date();
 function Footer() {
+  var date = new Date();
   return (
     <div className="footer mb-0 text-light mt-5">
       <div className="container">
         <h4>this is my demo movies app</h4>
-        <h6>{date.getFullYear}</h6>
+        <h6>{date.toDateString()}</h6>
       </div>
     </div>
   );
